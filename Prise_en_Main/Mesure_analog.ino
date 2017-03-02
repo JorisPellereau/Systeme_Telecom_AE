2d134e68f3c30a227a7e13467b6e856a13439af2
Joris P <jor.pell@hotmail.fr>
2017-03-02 15:10:02 +0100

    Code

---
+++ Prise_en_Main/Mesure_analog.ino
@@ -0,0 +1,24 @@
+/*
+ * Mesure sur ka voie analogique 0
+ * Conversion en volt
+ * Affichage resultat
+ */
+#define Pin_read_0 0
+int val_lue = 0 ;
+float val_volt = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  val_lue = analogRead(Pin_read_0);
+  val_volt = val_lue*5.0/1023;
+  Serial.print("Val num : " );
+  Serial.println(val_lue );
+  Serial.print("Val nvolt : " );
+  Serial.println(val_volt , 4);
+  delay(200);
+
+}
