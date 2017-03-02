9dd22fab5f0e0c193de70fcee5d671ad58fbe359
Joris P <jor.pell@hotmail.fr>
2017-03-02 17:28:13 +0100

    Code

---
+++ Prise_en_Main/COnv_DAC.ino
@@ -0,0 +1,22 @@
+/*
+ * Convertisseur numerique analogique + R2R
+ */
+ #define b0 0
+ #define b1 1
+ #define b2 2
+ char i = 0;
+ void setup() {
+  DDRD = 0x07;
+}
+
+void loop() {
+  for(i = 0 ; i <= 7 ; i++) {
+    PORTD = i;
+    delay(10);
+  }
+  for(i = 7 ; i >= 0 ; i--) {
+    PORTD = i;
+    delay(10);
+  }
+
+}
