25b817821fd719f7c0497361f0919f30adf095b9
Joris P <jor.pell@hotmail.fr>
2017-03-02 14:51:05 +0100

    Premiers codes

---
+++ Prise_en_Main/Affich_bjr.ino
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void setup() {
+  
+  Serial.begin(9600);     // Init Com serie à 9600 bauds
+}
+
+void loop() {
+  
+    Serial.println("Hello world");        // Affichage dans la console
+    delay(50);
+}
---
+++ Prise_en_Main/On_off_Pin_2.ino
@@ -0,0 +1,25 @@
+// On off sur ka pin n°2
+#define Pin_2 2
+
+void setup() {
+  
+  /*pinMode(Pin_2 , OUTPUT);
+  digitalWrite(Pin_2 , LOW);*/
+  //Avec les PORTs PIn2 => sur PORT D
+  DDRD = DDRD | 0x04;
+}
+
+void loop() {
+  
+  /*digitalWrite(Pin_2 , HIGH);
+  //delay(500);
+  digitalWrite(Pin_2 , LOW);
+  //delay(500);*/
+  
+  
+  PORTD = PORTD | 0x04;
+  //delay(250);
+  PORTD = PORTD & 0xFB; // Observation : Pas même temps en haut qu'en bas !!!! => execution instruction plus long
+  //delay(250);
+  
+}
